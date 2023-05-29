6c09d300ad7388b63ea04391cdf6bf5922bb76f8
Rob F <122385079+rfischer85@users.noreply.github.com>
2023-05-29 19:09:34 -0400

    First commit
    Added all the test data and requirements.txt

--- handyman_blog/views.py
+++ handyman_blog/views.py
@@ -5,4 +5,5 @@ handyman_blog/views.py
 from django.http import HttpResponse
 
 def index(request):
+    """function to return a message on an HTTP request"""
     return HttpResponse('Hello, world! This is my Handyman blog')
